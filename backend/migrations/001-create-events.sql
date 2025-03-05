8fa344a7ee6e6c068403a66e0c882179400b0e5c
geotsa <gtsagiannis@gmail.com>
2025-03-05 18:13:06 +0200

    Add riddle, wikipediaUrl, and details columns to events table and update seed data

--- backend/migrations/001-create-events.sql
+++ backend/migrations/001-create-events.sql
@@ -1,11 +1,13 @@
--- Enable the uuid-ossp extension for generating UUIDs (if not already enabled)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS events (
-   id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   date TIMESTAMP NOT NULL,
   location VARCHAR(255) NOT NULL,
   description TEXT,
-  imageUrl VARCHAR(255) NOT NULL
+  imageUrl VARCHAR(255) NOT NULL,
+  riddle TEXT,
+  wikipediaUrl TEXT,
+  details TEXT
 );
